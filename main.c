5b68554d78ba64616a865cbb4afaa5cb2052601f
sxclij <sxclij@gmail.com>
2024-04-09 07:49:02 +0900

    add main

--- main.c
+++ main.c
@@ -4,6 +4,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define hashtable_capacity (1<<27)
+
+static int32_t hashtable[hashtable_capacity];
+uint32_t hashtable_hash(int32_t key) {
+    key ^= key << 13;
+    key ^= key >> 17;
+    key ^= key << 5;
+    return key % hashtable_capacity;
+}
+int32_t* hashtable_find(int32_t key) {
+    uint32_t hash = hashtable_hash(key);
+    return &hashtable[hash];
+}
 
 int main() {
 }
